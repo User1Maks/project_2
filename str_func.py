d62ebe253e8ebfa9b021a97e37006215a4de78eb
Maksim Kamenev <maksim-kamenev96@mail.ru>
2024-02-24 19:17:08 +0300

    str_funcs_2

--- str_func.py
+++ str_func.py
@@ -1,6 +1,14 @@
 def str_funcs():
+    """Возвращает каждое слово в верхнем регистре"""
     user_input = input('Введите текст: ')
     return user_input.upper()
 
 
+def str_funcs_2():
+    """Возвращает каждую букву слова в верхнем регистре """
+    user_input = input('Введите текст: ')
+    return user_input.title()
+
+
 print(str_funcs())
+print(str_funcs_2())
